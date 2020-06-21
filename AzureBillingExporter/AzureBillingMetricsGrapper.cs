b255187580082f6b37ace53b9a0d6d809153255e
Pavel Pritchin <p.pritchin@dodopizza.com>
2020-06-21 20:35:05 +0300

    init

---
+++ AzureBillingExporter/AzureBillingMetricsGrapper.cs
@@ -0,0 +1,23 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Prometheus;
+
+namespace AzureBillingExporter
+{
+    public class AzureBillingMetricsGrapper
+    {
+        private static readonly Counter DailyCosts =
+            Metrics.CreateCounter("azure_billing_daily", "Today costs for subscription");
+
+        public async Task DownloadFromApi(CancellationToken cancel)
+        {
+            using var httpClient = new HttpClient();
+            // Probe a remote system.
+            var response = await httpClient.GetAsync("https://google.com", cancel);
+
+            // Increase a counter by however many bytes we loaded.
+            DailyCosts.Inc(response.Content.Headers.ContentLength ?? 0);
+        }
+    }
+}
---
+++ AzureBillingExporter/Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Prometheus;
+
+namespace AzureBillingExporter
+{
+    public class Startup
+    {
+        private static readonly AzureBillingMetricsGrapper _billingGrapper = new AzureBillingMetricsGrapper();
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            
+            Metrics.DefaultRegistry.AddBeforeCollectCallback(async (cancel) =>
+                {
+                    await _billingGrapper.DownloadFromApi(cancel);
+                });
+            
+            // ASP.NET Core 3 or newer
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapMetrics();
+            });
+        }
+    }
+}
